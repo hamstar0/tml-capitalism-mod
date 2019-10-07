758903f04678495e56af10b11cf2db486b708b78
hamstar0 <webutek@gmail.com>
2019-10-07 04:01:25 -0700

    v1.5.1 - Config ranges defined

--- Config.cs
+++ Config.cs
@@ -14,50 +14,53 @@ namespace Capitalism {
 		////////////////
 
 		[DefaultValue( true )]
-		public bool Enabled = true;
+		public bool Enabled { get; set; } = true;
 
 
+		[Header( "Formula for computing markup:\n"+
+			"  b=base value\n  t=total purchases\n  a=exponent factor (default 0.8)\n  c=divsor factor (default 50)\n \n"+
+			"b + ( (b *t)^0.8 ) / 50" )]
 		[Tooltip( "A factor for computing markup." )]
-		[Range( Single.MinValue, Single.MaxValue )]
+		[Range( -20f, 20f )]
 		[DefaultValue( 0.8f )]
-		public float MarkupExponent = 0.8f;
+		public float MarkupExponent { get; set; } = 0.8f;
 
 		[Tooltip( "A factor for computing markup." )]
-		[Range(Single.Epsilon, Single.MaxValue)]
+		[Range( Single.Epsilon, 1000f )]
 		[DefaultValue( 50f )]
-		public float MarkupDivisor = 50f;
+		public float MarkupDivisor { get; set; } = 50f;
 
 		[Tooltip("% markup if a Tax Collector NPC has moved in.")]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Range( 0f, 10f )]
 		[DefaultValue( 1.02f )]
-		public float TaxMarkupPercent = 1.02f;
+		public float TaxMarkupPercent { get; set; } = 1.02f;
 
 		[Tooltip("% markup after an NPC has died.")]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Range( 0f, 10f )]
 		[DefaultValue( 1.5f )]
-		public float InfuriationMarkupPercent = 1.5f;
+		public float InfuriationMarkupPercent { get; set; } = 1.5f;
 
 
-		[Tooltip("% that markup prices 'decay' twice per day.")]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Tooltip("% that markup prices 'decay' to, twice per day.")]
+		[Range( 0f, 10f )]
 		[DefaultValue( 0.95f )]
-		public float BiDailyDecayMarkdownPercent = 0.95f;
+		public float BiDailyDecayMarkdownPercent { get; set; } = 0.95f;
 
 
 		[Tooltip("% scale during a blood moon from female NPCs.")]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Range( 0f, 10f )]
 		[DefaultValue( 1.1f )]
-		public float FemaleBloodMoonSellPricePercent = 1.1f;
+		public float FemaleBloodMoonSellPricePercent { get; set; } = 1.1f;
 
 		[Tooltip( "% scale from a 'lovestruck' NPC." )]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Range( 0f, 10f )]
 		[DefaultValue( 0.9f )]
-		public float LovestruckSellPricePercent = 0.9f;
+		public float LovestruckSellPricePercent { get; set; } = 0.9f;
 
 		[Tooltip( "% scale from a 'stinky' NPC." )]
-		[Range( Single.Epsilon, Single.MaxValue )]
+		[Range( 0f, 10f )]
 		[DefaultValue( 1.1f )]
-		public float StinkySellPricePercent = 1.1f;
+		public float StinkySellPricePercent { get; set; } = 1.1f;
 
 
 
