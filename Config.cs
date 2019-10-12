c43f62ef864f701df4ad8f972d686e93bb778bd3
hamstar0 <webutek@gmail.com>
2019-10-11 18:01:40 -0700

    v1.5.1 - Implemented FloatInputElement for config

--- Config.cs
+++ Config.cs
@@ -1,4 +1,5 @@
-﻿using HamstarHelpers.Helpers.User;
+﻿using HamstarHelpers.Classes.UI.ModConfig;
+using HamstarHelpers.Helpers.User;
 using Newtonsoft.Json;
 using System;
 using System.ComponentModel;
@@ -39,51 +40,51 @@ namespace Capitalism {
 		[Tooltip( "A factor for computing markup." )]
 		[Range( 0f, 5f )]
 		[DefaultValue( 0.8f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float MarkupExponent { get; set; } = 0.8f;
 
 		[Tooltip( "A factor for computing markup." )]
 		[Range( Single.Epsilon, 1000f )]
 		[DefaultValue( 50f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float MarkupDivisor { get; set; } = 50f;
 
 		[Tooltip("% markup if a Tax Collector NPC has moved in.")]
 		[Range( 0f, 10f )]
 		[DefaultValue( 1.02f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float TaxMarkupPercent { get; set; } = 1.02f;
 
 		[Tooltip("% markup after an NPC has died.")]
 		[Range( 0f, 10f )]
 		[DefaultValue( 1.5f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float InfuriationMarkupPercent { get; set; } = 1.5f;
 
 
 		[Tooltip("% that markup prices 'decay' to, twice per day.")]
 		[Range( 0f, 10f )]
 		[DefaultValue( 0.95f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float BiDailyDecayMarkdownPercent { get; set; } = 0.95f;
 
 
 		[Tooltip("% scale during a blood moon from female NPCs.")]
 		[Range( 0f, 10f )]
 		[DefaultValue( 1.1f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float FemaleBloodMoonSellPricePercent { get; set; } = 1.1f;
 
 		[Tooltip( "% scale from a 'lovestruck' NPC." )]
 		[Range( 0f, 10f )]
 		[DefaultValue( 0.9f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float LovestruckSellPricePercent { get; set; } = 0.9f;
 
 		[Tooltip( "% scale from a 'stinky' NPC." )]
 		[Range( 0f, 10f )]
 		[DefaultValue( 1.1f )]
-		//[CustomModConfigItem( typeof(ConfigFloatInput) )]
+		[CustomModConfigItem( typeof( FloatInputElement ) )]
 		public float StinkySellPricePercent { get; set; } = 1.1f;
 
 
